e08a0172b073ca52e18298be02a81840f7dfdb23
abdullah-3024 <abdullah.al.adib@g.bracu.ac.bd>
2025-04-16 10:11:38 +0600

    node modules deleted

--- ecoloop_backend/server.js
+++ ecoloop_backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://ecoloop:cse470@ecoloop.qopm4aj.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('✅ Connected to MongoDB Atlas'))
-  .catch((err) => console.error('❌ Connection error:', err));
+}).then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Connection error:', err));
 
 app.listen(4000);
