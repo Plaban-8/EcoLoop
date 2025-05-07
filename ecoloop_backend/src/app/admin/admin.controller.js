e50c0db44eb1621e9bcfaa5da3320ddc7c6d3ccd
Abdullah Al Adib <mail4adib@gmail.com>
2025-05-07 17:31:05 +0600

    admin incompleted

---
+++ ecoloop_backend/src/app/admin/admin.controller.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+export const adminController = Router();
+
+adminController.post('/', (req,res)=>{
+  
+})
--- ecoloop_backend/src/app/controller.js
+++ ecoloop_backend/src/app/controller.js
@@ -1,9 +1,11 @@
+import { adminController } from "./admin/admin.controller.js";
 import { authController } from "./auth/auth.controller.js";
 import { donationController } from "./donation/donation.controller.js";
 import { foodController } from "./food/food.controller.js";
 
 export const controller = (app) => {
   app.use("/auth", authController);
-  app.use("/donation", donationController)
-  app.use("/food", foodController)
+  app.use("/donation", donationController);
+  app.use("/food", foodController);
+  app.use("/admin", adminController);
 };
