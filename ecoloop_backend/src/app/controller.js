92aa8252bc29b4b3eba099b3ba5d898a412fd4df
Abdullah Al Adib <mail4adib@gmail.com>
2025-05-09 15:30:26 +0600

    package jdon tweeked

--- ecoloop_backend/src/app/controller.js
+++ ecoloop_backend/src/app/controller.js
@@ -3,6 +3,7 @@ import { authController } from "./auth/auth.controller.js";
 import { donationController } from "./donation/donation.controller.js";
 import { foodController } from "./food/food.controller.js";
 import { profileController } from "./user/profile/profile.controller.js";
+import { settingsController } from "./user/settings/settings.controller.js";
 
 export const controller = (app) => {
   app.use("/auth", authController);
@@ -10,4 +11,5 @@ export const controller = (app) => {
   app.use("/food", foodController);
   app.use("/admin", adminController);
   app.use("/user/profile", profileController);
+  app.use("/user/settings", settingsController);
 };
